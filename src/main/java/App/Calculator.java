c025de3e515dea6dea698d5c505a307e4dd8354f
Manali0704 <manaliadhvaryu@gamil.com>
2023-08-01 21:29:58 +0530

    first commit

---
+++ src/main/java/App/Calculator.java
@@ -0,0 +1,19 @@
+package App;
+
+public class Calculator {
+	
+	public int add(int i, int j)
+	{
+		return i+j;
+		
+	}
+	
+	public int multiply(int i, int j)
+	{
+		return i*j;
+		
+	}
+	
+	
+
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,17 @@
+import App.Calculator;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void test() {
+		
+			
+			int exp = 30;
+			Calculator cal = new Calculator();
+			int act = cal.add(10, 20);
+			Assert.assertEquals(exp, act);
+	}
+
+}
